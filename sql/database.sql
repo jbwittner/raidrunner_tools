68115b84819010366bc8a580902e0e4ca9240fea
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2024-06-20 23:33:08 +0200

    update sql

--- sql/database.sql
+++ sql/database.sql
@@ -16,20 +16,45 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+ALTER TABLE ONLY public."character" DROP CONSTRAINT fkcharacter_playablerace;
+ALTER TABLE ONLY public.wow_account DROP CONSTRAINT fk_wowaccount_user;
 ALTER TABLE ONLY public.playable_specialization DROP CONSTRAINT fk_playableclass_playablespecialization;
+ALTER TABLE ONLY public."character" DROP CONSTRAINT fk_character_wowaccount;
+ALTER TABLE ONLY public."character" DROP CONSTRAINT fk_character_realm;
+ALTER TABLE ONLY public."character" DROP CONSTRAINT fk_character_playableclass;
+ALTER TABLE ONLY public.wow_account DROP CONSTRAINT wow_account_pkey;
 ALTER TABLE ONLY public.user_account DROP CONSTRAINT user_account_pkey;
 ALTER TABLE ONLY public.realm DROP CONSTRAINT realm_pkey;
 ALTER TABLE ONLY public.playable_specialization DROP CONSTRAINT playable_specialization_pkey;
 ALTER TABLE ONLY public.playable_race DROP CONSTRAINT playable_race_pkey;
 ALTER TABLE ONLY public.playable_class DROP CONSTRAINT playable_class_pkey;
+ALTER TABLE ONLY public."character" DROP CONSTRAINT character_pkey;
+DROP TABLE public.wow_account;
 DROP TABLE public.user_account;
 DROP TABLE public.realm;
 DROP TABLE public.playable_specialization;
 DROP TABLE public.playable_race;
 DROP TABLE public.playable_class;
+DROP TABLE public."character";
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
+
+--
+-- Name: character; Type: TABLE; Schema: public; Owner: raidrunner_user
+--
+
+CREATE TABLE public."character" (
+    account_id integer NOT NULL,
+    character_id integer NOT NULL,
+    playable_class_id integer NOT NULL,
+    playable_race_id integer NOT NULL,
+    realm_id integer NOT NULL,
+    character_name character varying(255) NOT NULL
+);
+
+
+ALTER TABLE public."character" OWNER TO raidrunner_user;
 
 --
 -- Name: playable_class; Type: TABLE; Schema: public; Owner: raidrunner_user
@@ -99,6 +124,26 @@ CREATE TABLE public.user_account (
 ALTER TABLE public.user_account OWNER TO raidrunner_user;
 
 --
+-- Name: wow_account; Type: TABLE; Schema: public; Owner: raidrunner_user
+--
+
+CREATE TABLE public.wow_account (
+    account_id integer NOT NULL,
+    user_id integer NOT NULL
+);
+
+
+ALTER TABLE public.wow_account OWNER TO raidrunner_user;
+
+--
+-- Name: character character_pkey; Type: CONSTRAINT; Schema: public; Owner: raidrunner_user
+--
+
+ALTER TABLE ONLY public."character"
+    ADD CONSTRAINT character_pkey PRIMARY KEY (character_id);
+
+
+--
 -- Name: playable_class playable_class_pkey; Type: CONSTRAINT; Schema: public; Owner: raidrunner_user
 --
 
@@ -139,11 +184,59 @@ ALTER TABLE ONLY public.user_account
 
 
 --
+-- Name: wow_account wow_account_pkey; Type: CONSTRAINT; Schema: public; Owner: raidrunner_user
+--
+
+ALTER TABLE ONLY public.wow_account
+    ADD CONSTRAINT wow_account_pkey PRIMARY KEY (account_id);
+
+
+--
+-- Name: character fk_character_playableclass; Type: FK CONSTRAINT; Schema: public; Owner: raidrunner_user
+--
+
+ALTER TABLE ONLY public."character"
+    ADD CONSTRAINT fk_character_playableclass FOREIGN KEY (playable_class_id) REFERENCES public.playable_class(playable_class_id);
+
+
+--
+-- Name: character fk_character_realm; Type: FK CONSTRAINT; Schema: public; Owner: raidrunner_user
+--
+
+ALTER TABLE ONLY public."character"
+    ADD CONSTRAINT fk_character_realm FOREIGN KEY (realm_id) REFERENCES public.realm(realm_id);
+
+
+--
+-- Name: character fk_character_wowaccount; Type: FK CONSTRAINT; Schema: public; Owner: raidrunner_user
+--
+
+ALTER TABLE ONLY public."character"
+    ADD CONSTRAINT fk_character_wowaccount FOREIGN KEY (account_id) REFERENCES public.wow_account(account_id);
+
+
+--
 -- Name: playable_specialization fk_playableclass_playablespecialization; Type: FK CONSTRAINT; Schema: public; Owner: raidrunner_user
 --
 
 ALTER TABLE ONLY public.playable_specialization
     ADD CONSTRAINT fk_playableclass_playablespecialization FOREIGN KEY (playable_class_id) REFERENCES public.playable_class(playable_class_id);
+
+
+--
+-- Name: wow_account fk_wowaccount_user; Type: FK CONSTRAINT; Schema: public; Owner: raidrunner_user
+--
+
+ALTER TABLE ONLY public.wow_account
+    ADD CONSTRAINT fk_wowaccount_user FOREIGN KEY (user_id) REFERENCES public.user_account(user_id);
+
+
+--
+-- Name: character fkcharacter_playablerace; Type: FK CONSTRAINT; Schema: public; Owner: raidrunner_user
+--
+
+ALTER TABLE ONLY public."character"
+    ADD CONSTRAINT fkcharacter_playablerace FOREIGN KEY (playable_race_id) REFERENCES public.playable_race(playable_race_id);
 
 
 --
